ce3ed57ac8f6dd9d6cdd39e9b75dc5f98115fc09
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-16 18:17:15 -0400

    feat: initial commit for moving to react.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css'
+import {IntroBody} from "./components/IntroBody";
+
+export default function App() {
+
+  return (
+    <React.Fragment>
+      <IntroBody/>
+    </React.Fragment>
+  );
+}
