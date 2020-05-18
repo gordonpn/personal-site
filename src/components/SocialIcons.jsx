2a829970f20f3a8a68912b590c7366e8400d079d
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-18 14:19:46 -0400

    refactor(SocialIcons): reduce duplicate code

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookMessenger,
+  faGithub,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { createUseStyles } from "react-jss";
 import * as PropTypes from "prop-types";
@@ -12,46 +17,33 @@ const useStyles = createUseStyles({
   },
 });
 
+const data = [
+  { icon: faGithub, link: "https://github.com/gordonpn" },
+  { icon: faFacebookMessenger, link: "https://www.messenger.com/t/Gordonpn" },
+  { icon: faInstagram, link: "https://www.instagram.com/gordonpn/" },
+  { icon: faEnvelope, link: "mailto:contact@gordon-pn.com" },
+  { icon: faLinkedinIn, link: "https://www.linkedin.com/in/gordonpn" },
+];
+
 export default function SocialIcons(props) {
   const classes = useStyles();
   const { size } = props;
-  return (
-    <>
-      <a
-        href="https://www.linkedin.com/in/gordonpn/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon
-          icon={faLinkedinIn}
-          size={size}
-          className={classes.icons}
-        />
-      </a>
-      <a
-        href="https://github.com/gordonpn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon
-          icon={faGithub}
-          size={size}
-          className={classes.icons}
-        />
-      </a>
-      <a
-        href="mailto:contact@gordon-pn.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          size={size}
-          className={classes.icons}
-        />
-      </a>
-    </>
-  );
+
+  const loadData = data.map((socialItem) => {
+    return (
+      <>
+        <a href={socialItem.link} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon
+            icon={socialItem.icon}
+            size={size}
+            className={classes.icons}
+          />
+        </a>
+      </>
+    );
+  });
+
+  return <>{loadData}</>;
 }
 
 SocialIcons.propTypes = {
